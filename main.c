fde2e2f0edcdd5013218bc44bea345d36618653f
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-02-06 22:48:32 -0500

    Implemented dict_contains and fixed some bugs with the dictionary

--- main.c
+++ main.c
@@ -217,16 +217,39 @@ typedef struct NodeT {
     struct NodeT *children;
 } Node;
 
-Node *dict_init(Node *head, int numColors) {
-    head->val = result->index = -1;
+void dict_init(Node *head, const int numColors) {
+    head->val = head->index = -1;
     head->numChildren = numColors;
     head->children = malloc(sizeof(Node)*numColors);
 
     int i;
     for(i = 0; i < numColors; i++) {
-        head->children[i].val = result->children[i].index = i;
+        head->children[i].val = head->children[i].index = i;
         head->children[i].numChildren = 0;
     }
+}
+
+void node_add(Node *node, const int value, const int pos) {
+    //this is the first value I can use in GIF LZW
+    static int currIndex = LZW_FIRST_INDEX;
+
+    Node *temp = malloc(sizeof(Node)*(node->numChildren + 1));
+    //copy all of the nodes with a value less than this one
+    memcpy(temp, node->children, sizeof(Node)*pos);
+
+    //write the new node
+    temp[pos].val = value;
+    temp[pos].index = ++currIndex;
+    temp[pos].numChildren = 0;
+
+    //copy the rest of the children into the new array
+    memcpy(temp+pos+1, node->children+pos, sizeof(Node)*(node->numChildren - pos));
+
+    if(node->numChildren > 0) {
+        free(node->children);
+    }
+    node->children = temp;
+    node->numChildren++;
 }
 
 /**
@@ -239,36 +262,22 @@ Node *dict_init(Node *head, int numColors) {
  * @return the index of the node above the new value (the LZW code to output)
  */
 int dict_add(Node *head, const int *value, const int valuec) {
-    //this is the first value I can use in GIF LZW
-    static int currIndex = LZW_FIRST_INDEX;
+    if(head->numChildren == 0 && valuec == 1) {
+        node_add(head, value[0], 0);
+        return head->index;
+    }
 
     int i;
     for(i = 0; i < head->numChildren; i++) {
-        if(value[0] == head->children[i].val) {
+        if(head->children[i].val == value[0]) {
             if(valuec == 1) { //the child already exists
                 return head->index;
             }else{ //we have to go deeper
                 return dict_add(&head->children[i], value + 1, valuec - 1);
             }
-        }else if(value[0] > head->children[i].val) { //val is not a child of head
+        }else if(head->children[i].val > value[0]) { //val is not a child of head
             if(valuec == 1) { //add the node as head's child
-                Node *temp = malloc(sizeof(Node)*(head->numChildren + 1));
-                //copy all of the nodes with a value less than this one
-                memcpy(temp, head->children, sizeof(Node)*(i+1));
-
-                //write the new node
-                head->children[i+1].val = value[0];
-                head->children[i+1].index = ++currIndex;
-                head->children[i+1].numChildren = 0;
-
-                //copy the rest of the children into the new array
-                memcpy(temp+i+2, head->children+i+1,
-                        sizeof(Node)*(head->numChildren - i - 1));
-
-                free(head->children);
-                head->children = temp;
-                head->numChildren++;
-
+                node_add(head, value[0], i);
                 return head->index;
             }else{
                 //technically I could handle this case but it won't happen in LZW
@@ -284,13 +293,30 @@ int dict_add(Node *head, const int *value, const int valuec) {
 /**
  * @return 1 if the dictionary contains the value 0 otherwise
  */
-int dict_contains(Node *head, int *value, int valuec) {
-    
+int dict_contains(const Node *head, const int *value, const int valuec) {
+    if(valuec <= 0) {
+        return 0;
+    }
+
+    int i;
+    for(i = 0; i < head->numChildren; i++) {
+        if(head->children[i].val == value[0]) {
+            if(valuec == 1) {
+                return 1;
+            }else{
+                return dict_contains(&head->children[i], value + 1, valuec - 1);
+            }
+        }else if(head->children[i].val > value[0]) {
+            return 0;
+        }
+    }
+
+    return 0;
 }
 
 void dict_free(Node *head) {
     int i;
-    for(int i = 0; i < head->numChildren; i++) {
+    for(i = 0; i < head->numChildren; i++) {
        if(head->children[i].numChildren > 0) {
             dict_free(&head->children[i]);
        }
