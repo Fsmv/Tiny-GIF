f35d90bd593ef97e6b7ddfa15c8f7dc55c48e795
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-03-28 23:37:19 -0400

    Created a new API in LZW to compress one byte at a time

--- include/LZW.h
+++ include/LZW.h
@@ -3,9 +3,16 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include "Dictionary.h"
 
 //the maximum index for gifs
 #define MAX_INDEX 0xFFF
+
+typedef struct {
+    Dictionary dict;
+    uint8_t *currSym;
+    uint8_t alphabetSize;
+} LZW;
 
 /**
  * Compresses a string with the LZW algorithm
@@ -30,5 +37,26 @@ extern void LZW_Compress(const char *string, size_t stringc, uint16_t **code, si
  * @param alphabetSize the number of possible initial codes
  */
 extern void LZW_Decompress(const uint16_t *code, const size_t codec, char **string, uint8_t alphabetSize);
+
+/**
+ * Compresses data one byte at a time
+ *
+ * @param data byte to compress
+ * @param state the state, alphabetSize must be initialized, dict and currSym
+ * must be NULL for the first byte
+ * @return the compressed data value (or 0xFFFF if nothing was output)
+ */
+extern uint16_t LZW_CompressOne(const char data, LZW *state);
+
+/**
+ * Initializes LZW state
+ *
+ * dict = currSym = NULL;
+ * alphabetSize = alphabetSize;
+ */
+inline void LZW_Init(uint8_t alphabetSize, LZW *state) {
+    state->dict = state->currSym = NULL;
+    state->alphabetSize = alphabetSize;
+}
 
 #endif
